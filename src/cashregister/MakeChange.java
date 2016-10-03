dbd6f6021d6077a5a5cbbc3c470fdee6c24ee778
Brian Ipock <ipock@Macs-MacBook-Pro.local>
2016-10-02 20:33:50 -0600

    Make Change homework

--- src/cashregister/MakeChange.java
+++ src/cashregister/MakeChange.java
@@ -14,23 +14,27 @@ public class MakeChange {
 		Scanner register = new Scanner(System.in);
 		System.out.print("Hey Clerk!! Enter the price of the item, quit being stoned!");
 		float cost = register.nextFloat();
-		cost = (float)((int)(cost*100))/100;
+		cost = (float)(cost*100)/100;
 		System.out.printf("The item price is: $%.2f", cost);
 		System.out.println();
 		System.out.print("How much money did the customer just hand you?");
 		float payment = register.nextFloat();
+		payment = (float)(payment*100)/100;
 		System.out.println();
 		System.out.printf("The item price is: $%.2f", cost);
 		System.out.println();
 		System.out.printf("The customer paid: $%.2f", payment);
 		System.out.println();
 
+		register.close();
 		getMoney(payment, cost);
 
 	}// makeTransaction
 
 	public static void getMoney(float payment, float cost) {
 		float change = payment - cost;
+		change = (float)(change*100)/100;
+
 		if (cost > payment) {
 			System.err.println("You didn't pay enough money,");
 			System.err.println(" I will need more money if you want to buy this item.");
@@ -50,8 +54,8 @@ public class MakeChange {
 
 	public static void giveChange(float payment, float cost, float change) {
 		float billsCents[] = { 20f, 10f, 5f, 1f, 0.25f, 0.10f, 0.05f, 0.01f };
-		String values[] = { "Twenty", "Ten", "Five", "One", "Quarter", "Dime", "Nickel", "Penny" };
-		String valuesPlural[] = { "Twenties", "Tens", "Fives", "Ones", "Quarters", "Dimes", "Nickels", "Pennies" };
+		String values[] = { "Twenty dollar bill", "Ten dollar bill", "Five dollar bill", "One dollar bill", "Quarter", "Dime", "Nickel", "Penny" };
+		String valuesPlural[] = { "Twenty dollar bills", "Ten dollar bills", "Five dollar bills", "One dollar bills", "Quarters", "Dimes", "Nickels", "Pennies" };
 
 		for (int i = 0; i < billsCents.length; i++) {
 			if (((int) (change / billsCents[i])) == 1) {
